770057bdb9b9e591991f20011648185e23260622
darellanodev <darellano.aliaga@gmail.com>
2024-11-08 19:27:18 +0100

    Install jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/'],
+    testMatch: ['**/*.test.js'],
+  };
