53bd807326921972080d5271a24190272512b0a0
darellanodev <darellano.aliaga@gmail.com>
2024-11-10 19:14:24 +0100

    refactor extract to function

--- src/statement.js
+++ src/statement.js
@@ -14,26 +14,8 @@ function statement (invoice, games) {
 
     for (let playedGame of invoice.playedGames) {
         const game = games[playedGame.id]
-
-        let thisAmount = 0
         
-        switch (game.genre) {
-            case "clicker":
-                thisAmount = 2
-                if (playedGame.days > 10) {
-                    thisAmount += 10 * (playedGame.days - 10)
-                }
-                break
-            case "towerDefense":
-                thisAmount = 3
-                if (playedGame.days > 8) {
-                    thisAmount += 12 * (playedGame.days - 10)
-                }
-                break 
-            default:
-                throw new Error(`unknown type: ${game.genre}`);
-                
-        }
+        thisAmount = getAmount(game, playedGame)
 
         // add volume credits
         volumeCredits += Math.max(playedGame.days - 5, 0)
@@ -54,6 +36,28 @@ function statement (invoice, games) {
     result += `You earned ${volumeCredits} credits\n`
     return result
 
+
+    function getAmount(game, playedGame) {
+        let thisAmount = 0
+        switch (game.genre) {
+            case "clicker":
+                thisAmount = 2
+                if (playedGame.days > 10) {
+                    thisAmount += 10 * (playedGame.days - 10)
+                }
+                break
+            case "towerDefense":
+                thisAmount = 3
+                if (playedGame.days > 8) {
+                    thisAmount += 12 * (playedGame.days - 10)
+                }
+                break
+            default:
+                throw new Error(`unknown type: ${game.genre}`)
+
+        }
+        return thisAmount
+    }
 }
 
 module.exports = statement
